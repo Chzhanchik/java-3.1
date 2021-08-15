f14683619a13822a21164e30124cd64a05486d1b
chzhanchik <chzhanchik@gmail.com>
2021-08-15 21:45:15 +0300

    origin

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 5000;
+        int count = 20;
+        int miles = 1;
+        int totalBonus = price / count * miles;
+        System.out.println(totalBonus);
+    }
+}
